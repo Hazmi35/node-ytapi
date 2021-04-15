f5c32c917c7b3748e0616f1cbc2a2aabbbf4830a
Hazmi35 <contact@hzmi.xyz>
2021-04-15 22:19:58 +0700

    feat: Create YouTube class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("It works!");
+export class YouTube {
+    public constructor(public readonly key: string) {
+        if (typeof key !== "string") throw new Error("The provided YouTube API key is not a string");
+
+        Object.defineProperty(this, "key", { enumerable: false });
+    }
+}
